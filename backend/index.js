03acd8526868fbbb0b89f92d141dc6ba16f346fe
4pd4stantone <victor.stanton@gmail.com>
2025-11-18 10:13:29 -0500

    added a route for danceStudioSocials

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config'
 import connectDB from './db.js';
+import DanceStudioSocials from './models/socials.js';
 
 const app = express();
 
@@ -11,6 +12,16 @@ app.use(cors())
 
 app.get('/', (req, res) => {
     res.json('You are connected to the server on port: ' + port)
+})
+
+app.get('/danceStudioSocials', async (req, res) => {
+    try {
+    const danceStudioSocials = await DanceStudioSocials.find({});
+    res.status(200).json(danceStudioSocials);
+    } catch(e) {
+        console.log(e.message);
+        res.status(400).json({ error: e.message})
+    }
 })
 
 app.listen(8080, () => {
